40045b1f00c8d5ff6525bd2b16c106debccb2d1f
Janaina Tavares <112178884+profJana@users.noreply.github.com>
2023-05-29 21:19:30 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+
+
+function setup() {
+  canvas = createCanvas(280, 280);
+  canvas.center();
+  background("white");
+  //canvas.mouseReleased(classifyCanvas);
+  synth = window.speechSynthesis;
+}
+
+function preload() {
+
+
+  classifier = ml5.imageClassifier('DoodleNet');
+}
+
+
+
+function clearCanvas() {
+
+  background("white");
+}
+
+function draw() {
+
+  //Defina strokeWeight como 13
+  strokeWeight(13);
+  //Defina a cor de stroke como preto
+  stroke(0);
+  //Se o moude for clicado, desenhe uma linha entre a posição antiga e atual do mouse
+  if (mouseIsPressed) {
+    line(pmouseX, pmouseY, mouseX, mouseY);
+  }
+}
+
+function verificar(){
+  classifier.classify(canvas, gotResult);
+}
+
+//function classifyCanvas() {
+  //classifier.classify(canvas, gotResult);
+//}
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  }
+  console.log(results);
+  var result = results[0].label;
+  document.getElementById('label').innerHTML = 'Nome: ' + result.replace('_', ' ');
+
+
+  document.getElementById('confidence').innerHTML = 'Precisão: ' + Math.round(results[0].confidence * 100) + '%';
+
+  utterThis = new SpeechSynthesisUtterance(result.replace('_', ' '));
+  synth.speak(utterThis);
+  
+}
+
+
